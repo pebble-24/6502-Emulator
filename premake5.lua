99844f33d13c15951213094284b91203e138d80d
Daniel Horton <daniel.horton110@gmail.com>
2023-12-02 22:17:45 +0000

    Modified files for adding a 6502 assembler

--- premake5.lua
+++ premake5.lua
@@ -6,7 +6,23 @@ project "6502-Emulator"
    language "C"
    targetdir "bin/%{cfg.buildcfg}"
 
-   files { "src/main.c", "src/instruction.c", "src/io.c", "src/memory.c", "src/cpu.c", "src/computer.c" }
+   files { "src/main.c", "src/instructions.c", "src/io.c", "src/memory.c", "src/cpu.c", "src/computer.c" }
+   links {  }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
+
+project "6502-Assembler"
+   kind "ConsoleApp"
+   language "C"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "tools/assembler/assembler.c" }
    links {  }
 
    filter "configurations:Debug"
