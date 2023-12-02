2fd2bc5427213131931b2c6d7048068349aca7c1
Daniel Horton <daniel.horton110@gmail.com>
2023-12-02 22:19:23 +0000

    Added an assembler

---
+++ tools/assembler/assembler.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "types.h"
+
+int main(u32 argc, char **argv)
+{
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ tools/assembler/types.h
@@ -0,0 +1,30 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* Signed int types */
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+/* Unsigned int types */
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+// Float Types
+typedef float f32;
+typedef double f64;
+
+/* Boolean Types */
+typedef int b32;
+typedef bool b8;
+
+typedef u8 byte;
+typedef u16 word;
+
+#endif
