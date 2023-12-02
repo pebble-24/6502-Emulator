0f2e509ee24e90c45c9434f277636d7d7711865f
Daniel Horton <daniel.horton110@gmail.com>
2023-12-02 21:41:10 +0000

    Modified main.c for running a simple program

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <stdbool.h>
 
+#include "computer.h"
 #include "memory.h"
 #include "cpu.h"
 #include "instructions.h"
 
 int main(int argc, char **argv)
 {
-    printf("Hello, World!\n");
-    return 0;
+	struct cpu CPU;
+	struct memory RAM;
+	
+	run(&CPU, &RAM, true, 1);
+
+	return 0;
 }
