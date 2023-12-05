976d523cd6617fa36c7fce09ba512354c27562e3
Daniel Horton <daniel.horton110@gmail.com>
2023-12-05 11:04:55 +0000

    Added read and fetch functions for words and bytes

--- src/memory.c
+++ src/memory.c
@@ -6,3 +6,40 @@ void memory_clear(struct memory *memory)
 		memory->data[i] = 0x00;
 	}
 }
+
+/* Memory IO */
+byte memory_read_byte(struct memory *memory,  u16 index)
+{
+	return memory->data[index];
+}
+
+word memory_read_word(struct memory *memory,  u16 index)
+{
+	word result = memory_read_byte(memory, index);
+	result |= (memory_read_byte(memory, index + 1) << 8);
+	return result;
+}
+
+// TODO Implement memory write functions
+void memory_write_byte(struct memory *memory, u16 index)
+{
+
+}
+
+void memory_write_word(struct memory *memory, u16 index)
+{
+
+}
+
+/* Fetch */
+byte memory_fetch_byte(struct memory *memory,  u16 index, u64 *cycles)
+{
+	(*cycles)--;
+	return memory_read_byte(memory, index);
+}
+
+word memory_fetch_word(struct memory *memory,  u16 index, u64 *cycles)
+{
+	(*cycles) -= 2;
+	return memory_read_word(memory, index);
+}
--- src/memory.h
+++ src/memory.h
@@ -9,6 +9,16 @@ struct memory {
 	u8 data[MAX_MEMORY_SIZE];
 };
 
+/* Memory IO */
+byte memory_read_byte(struct memory *memory,  u16 index);
+word memory_read_word(struct memory *memory,  u16 index);
+void memory_write_byte(struct memory *memory, u16 index);
+void memory_write_word(struct memory *memory, u16 index);
+
+/* Fetch */
+byte memory_fetch_byte(struct memory *memory,  u16 index, u64 *cycles);
+word memory_fetch_word(struct memory *memory,  u16 index, u64 *cycles);
+
 void memory_clear(struct memory *memory);
 
 #endif
